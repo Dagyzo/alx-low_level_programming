8b02ba45964c8a47b01e64e62ea410258b521498
Douglas Nyakundi <douglasmigiro@gmail.com>
2022-12-08 12:57:39 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
-#include <studio.h>
-
+#include <stdio.h>
 /**
- * main - Entry Point
- *
+ * main - Entry
  * Return: Always 0
  */
-
 int main(void)
-{     
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
-
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }	
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,27 @@
+#include <stdio>
+
+
+
+/**
+ *
+ *  * main - Entry Point
+ *
+ *   *
+ *
+ *    * Return: Always 0
+ *
+ *     */
+
+
+
+int main(void)
+
+{     
+
+	        puts("\"with proper grammar, but the outcome is a piece of art");
+
+		        return (0);
+
+
+
+}	

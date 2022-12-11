4629c6ceb24c3ec1613074e72a01f55c1b650f86
Douglas Nyakundi <douglasmigiro@gmail.com>
2022-12-11 07:15:16 -0800

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry
+ * main - prints exactly and that piece of art is useful
  * Return: 1
  */
 int main(void)
 {
-	write(1, "and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 

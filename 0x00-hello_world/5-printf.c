d7274d2d6e014c12817bc0bbea28f80d18a552fa
Douglas Nyakundi <douglasmigiro@gmail.com>
2022-12-11 06:19:04 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,27 +1,11 @@
-#include <stdio>
-
-
-
+#include <stdio.h>
 /**
- *
- *  * main - Entry Point
- *
- *   *
- *
- *    * Return: Always 0
- *
- *     */
-
-
-
+ * main - Entry
+ * Return: Always 0
+ */
 int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
 
-{     
-
-	        puts("\"with proper grammar, but the outcome is a piece of art");
-
-		        return (0);
-
-
-
-}	
